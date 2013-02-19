8fed93feb03ce131ee0f5ae45166b2ea95551758
Sara Van Buskirk <svanbusk@unca.edu>
2013-02-19 17:55:34 -0500

    Make one tweet cycle onscreen.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,32 +1,45 @@
-var tweetArray = new Array(10);
-var top = 0;
+var userImage = new Array(10),
+  userName = new Array(10),
+  tweetText = new Array(10),
+  i;
+
+var isUndefined = function (tweetArray) {
+  "use strict";
+  for (i = 0; i <= 9; i += 1) {
+    if (tweetArray[i] === undefined) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+var queue = function (tweetArray, newTweet) {
+  "use strict";
+  for (i = 0; i <= 9; i += 1) {
+    if (i < 9) {
+      tweetArray[i] = tweetArray[i + 1];
+    } else {
+      tweetArray[i] = newTweet;
+    }
+  }
+};
 
 var main = function () {
   "use strict";
   var twitter = new ctwitter.CTwitter();
-  twitter.stream("statuses/filter", { lang: "en", track: ["javascript", "Douglas Crockford", "jslint"] }, function (stream) {
+  twitter.stream("statuses/filter", { lang: "en", track: ["javascript", "jslint"] }, function (stream) {
     stream.on("data", function (tweet) {
-      //$("#feed").append("<img src ='" + tweet.profile_image_url + "' />" + "<h3>" + tweet.from_user + "</h3>" + "<p>" + tweet.text + "</p>");
+      queue(userImage, tweet.profile_image_url);
+      queue(userName, tweet.from_user);
+      queue(tweetText, tweet.text);
+      for(i = 0; i <= 9; i++) {
+        if (tweetText[i] !== undefined) {
+          $("#feed").html("<img src ='" + userImage[i] + "' width='50' />" + "<h4>" + userName[i] + "</h4>" + "<p>" + tweetText[i] + "</p>");              
+        }
+      }
     });
   });
-}
-
-var queue = function (newTweet) {
-  var hopper = null;
-  for (var i = 0; i<=9; i+=1) {
-    hopper = tweetArray[i];
-    if (i < 9) {
-      tweetArray[i] = tweetArray[i+1];
-    } else {}
-      tweetArray[top] = newTweet;
-    }
-  }
-}
-
-var dequeue = function() {
-  
-  }
-}
+};
 
 $(document).ready(main);
-console.log("Hello world");
